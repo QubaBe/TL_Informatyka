ba68b263922300c40e9485c6f8876298f1dca48a
Oleksandr Bielskyi <amyntwr@gmail.com>
2026-05-13 09:49:27 +0200

    Modify waliduj_tablice to return match object
    Update waliduj_tablice function to return match object instead of boolean.

--- tablice_rejestracyjne.py
+++ tablice_rejestracyjne.py
@@ -6,8 +6,7 @@ import re
 active = True
 def waliduj_tablice(tekst):
   match = re.search("[A-Z][A-Z][A-Z]?\s\w\w\w\w\w*", tekst)
-  if len(match) > 0:
-    return True
+  return match
   
 while active:
   result = waliduj_tablice(input("Proszę wprowadzić ciąg znaków: "))
